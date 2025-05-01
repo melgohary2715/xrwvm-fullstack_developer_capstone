70471c6cffb77f83e0cb8ffba3b96feb1b845982
melgohary2715 <melgohary2715@gmail.com>
2025-05-01 16:02:34 -0400

    app.js in server

--- server/database/app.js
+++ server/database/app.js
@@ -58,18 +58,44 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+  try {
+    const dealers = await Dealerships.find();
+    res.json(dealers);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
+
+
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+  try {
+    const state = req.params.state;
+    const dealers = await Dealerships.find({ state: state });
+    res.json(dealers);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
+
+
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+  try {
+    const id = parseInt(req.params.id);
+    const dealer = await Dealerships.findOne({ id: id });
+    if (!dealer) {
+      return res.status(404).json({ error: "Dealer not found" });
+    }
+    res.json(dealer);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
+
+
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
